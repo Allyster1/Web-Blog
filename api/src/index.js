8e369c8d5774ca5d4b9dacdd276912090f08de83
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-11 00:53:53 +0200

    update index file
    Adding db connection,  routes, json middleware and use environment variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import connectDB from "./config/database.js";
+import router from "./routes.js";
+import "dotenv/config";
 
 const app = express();
 
-app.get("/", (req, res) => {
-   res.send("it works");
+await connectDB();
 
-   res.end();
-});
+app.use(express.json());
 
-app.listen(5000, () => console.log(`Server is running on http://localhost:5000`));
+app.use(router);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on http://localhost:${process.env.PORT}`)
+);
