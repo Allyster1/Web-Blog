440764ef33a26e7811e8880acf4f9802f6ed6f4b
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-11 02:57:21 +0200

    remove comments and logout request

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,35 +3,26 @@ import { register, login } from "../services/userService.js";
 
 const userController = Router();
 
-// Register
 userController.post("/register", async (req, res) => {
-  try {
-    const { email, password, rePass } = req.body;
-    const token = await register(email, password, rePass);
+   try {
+      const { email, password, rePass } = req.body;
+      const token = await register(email, password, rePass);
 
-    res.status(201).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+      res.status(201).json({ token });
+   } catch (error) {
+      res.status(400).json({ error: error.message });
+   }
 });
 
-// Login
 userController.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const token = await login(email, password);
+   try {
+      const { email, password } = req.body;
+      const token = await login(email, password);
 
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(401).json({ error: error.message });
-  }
-});
-
-// Logout
-userController.post("/logout", async (req, res) => {
-  res.send("logout");
-
-  res.end();
+      res.status(200).json({ token });
+   } catch (error) {
+      res.status(401).json({ error: error.message });
+   }
 });
 
 export default userController;
