6b5222cf993ec6a702ae33e86b3a0840784e4950
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-12 15:58:00 +0200

    Add global rate limiter middleware

--- api/src/index.js
+++ api/src/index.js
@@ -3,8 +3,9 @@ import express from "express";
 import corseMiddleware from "./config/cors.js";
 import cookieParser from "cookie-parser";
 
-import router from "./config/routes.js";
 import connectDB from "./config/database.js";
+import router from "./config/routes.js";
+import { globalRateLimiter } from "./middlewares/globalRateLimiter.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
@@ -12,13 +13,11 @@ const app = express();
 await connectDB();
 
 app.use(corseMiddleware);
-
 app.use(express.json());
-
 app.use(cookieParser());
 
+app.use(globalRateLimiter);
 app.use(router);
-
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => console.log(`Server is running on http://localhost:${process.env.PORT}`));
