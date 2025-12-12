e44fac0c3b22f08569234ec351b67cdd80df8259
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-12 11:28:11 +0200

    moving router to config file and adding /api prefix

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import connectDB from "./config/database.js";
-import router from "./routes.js";
+import router from "./config/routes.js";
 import "dotenv/config";
+
+import connectDB from "./config/database.js";
 
 const app = express();
 
@@ -11,6 +12,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running on http://localhost:${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => console.log(`Server is running on http://localhost:${process.env.PORT}`));
--- api/src/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userController from "../controllers/userController.js";
-
-const router = Router();
-
-router.use("/api/users", userController);
-
-export default router;
