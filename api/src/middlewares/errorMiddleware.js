3ec43b3ae0ec7dac5d93513e7604f4a65f59c7fb
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-12 15:14:33 +0200

    Improve error responses based on environment

--- api/src/middlewares/errorMiddleware.js
+++ api/src/middlewares/errorMiddleware.js
@@ -1,7 +1,19 @@
 export function errorMiddleware(err, req, res, next) {
    const status = err.status || 500;
-   const message = err.message || "Internal server error";
-   const details = err.details || undefined;
+
+   const message =
+      process.env.NODE_ENV === "production" ? (status === 500 ? "Internal server error" : err.message) : err.message;
+
+   const details = process.env.NODE_ENV === "production" ? undefined : err.stack || err.details;
+
+   //   console.error("Error occurred:", {
+   //     message: err.message,
+   //     stack: err.stack,
+   //     status,
+   //     path: req.originalUrl,
+   //     method: req.method,
+   //     body: req.body,
+   //   });
 
    res.status(status).json({ message, details });
 }
