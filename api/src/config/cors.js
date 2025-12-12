a0512aa008e9c428ceaa03b4b19437c5da0bf0da
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-12 15:13:54 +0200

    refactor spaces

--- api/src/config/cors.js
+++ api/src/config/cors.js
@@ -1,18 +1,15 @@
 import cors from "cors";
 
-const whitelist =
-  process.env.NODE_ENV === "production"
-    ? [process.env.FRONTEND_URL]
-    : ["http://localhost:5173"];
+const whitelist = process.env.NODE_ENV === "production" ? [process.env.FRONTEND_URL] : ["http://localhost:5173"];
 
 const corsMiddleware = cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allows Postman / server-to-server
-    if (whitelist.includes(origin)) return callback(null, true);
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+   origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allows Postman / server-to-server
+      if (whitelist.includes(origin)) return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
+   },
+   credentials: true,
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
 export default corsMiddleware;
