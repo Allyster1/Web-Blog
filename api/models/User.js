ed08c34d6cf0dd7d51c1395e47faa393786a687f
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-12 11:43:35 +0200

    Increase password min length and update email message
    Raised the minimum password length from 6 to 8 characters and updated the email validation error message to 'Invalid email address!'.

--- api/models/User.js
+++ api/models/User.js
@@ -2,35 +2,35 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: [true, "Email is required!"],
-      unique: true,
-      lowercase: true,
-      trim: true,
-      validate: {
-        validator: function (v) {
-          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
-        },
-        message: "Please enter a valid email address!",
+   {
+      email: {
+         type: String,
+         required: [true, "Email is required!"],
+         unique: true,
+         lowercase: true,
+         trim: true,
+         validate: {
+            validator: function (v) {
+               return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: "Invalid email address!",
+         },
       },
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required!"],
-      minLength: [6, "The password should be at least 6 characters long"],
-      select: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+      password: {
+         type: String,
+         required: [true, "Password is required!"],
+         minLength: [8, "The password should be at least 8 characters long"],
+         select: false,
+      },
+   },
+   {
+      timestamps: true,
+   }
 );
 
 userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 12);
+   if (!this.isModified("password")) return;
+   this.password = await bcrypt.hash(this.password, 12);
 });
 
 const User = model("User", userSchema);
