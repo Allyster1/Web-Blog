0d8909d5891de79a93fa1319c341867c4123299a
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-14 23:25:42 +0200

    adding mongodb localhost option in database config

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || process.env.DATABASE;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/myBlog_dev";
 
 export default async function connectDB() {
    try {
