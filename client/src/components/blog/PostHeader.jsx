671aa406fd9f095735d32d5a2fad8c45908af730
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-15 16:38:24 +0200

    adding heading components for cleaner layout

---
+++ client/src/components/blog/PostHeader.jsx
@@ -0,0 +1,10 @@
+export default function PopularPosts({ heading, text }) {
+   return (
+      <>
+         <header className="mt-10 text-center px-4 pb-10">
+            <h1 className="text-3xl font-bold text-[#212121] leading-8 pb-2">{heading}</h1>
+            <p className="">{text}</p>
+         </header>
+      </>
+   );
+}
