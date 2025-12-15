e90fc15f149b928a7b33ae3ffab3f6f76cf76adf
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-15 16:39:20 +0200

    create mainCard component - top level card in blog

---
+++ client/src/components/blog/MainCard.jsx
@@ -0,0 +1,39 @@
+import mainImage from "../../assets/mainImage.svg";
+import { Link } from "react-router";
+
+export default function MainCard() {
+   return (
+      <div className="cursor-pointer relative mt-12 w-auto max-w-3x1 mx-auto rounded-xl overflow-hidden">
+         <Link to="/auth/login">
+            <div className="relative w-full h-[300px] sm:h-[350px] md:aspect-21/9">
+               <img
+                  className="absolute inset-0 w-full h-full object-cover"
+                  src={mainImage}
+                  alt="A Foodie's Guide to Europe"
+               />
+
+               <div className="absolute inset-0 bg-linear-to-br opacity-60 from-[#6c927b] via-[#2d4436] to-[#6f8096cc]" />
+
+               <div className="absolute inset-0 flex flex-col p-4 sm:p-6 md:p-8 text-white">
+                  <div className="w-full md:w-1/2">
+                     <h1 className="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem] lg:text-[1.6rem] font-bold leading-tight">
+                        A Foodie's Guide to Europe: Best Culinary Experiences by Country
+                     </h1>
+                  </div>
+
+                  <div className="mt-auto w-full flex flex-col sm:flex-row justify-between items-start sm:items-end gap-2 sm:gap-0">
+                     <p className="text-sm md:text-base">Nov 13, 2025</p>
+
+                     <div className="flex items-center gap-3 bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 self-start sm:self-end">
+                        <div className="text-sm text-white">
+                           <p className="font-semibold">Allyster1</p>
+                           <p className="text-xs opacity-90">Author</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </Link>
+      </div>
+   );
+}
