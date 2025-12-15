b9f77705e8fb729922caa9f4df3ff53a0eb9c1ef
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-15 16:37:47 +0200

    refactor container sizing

--- client/src/layouts/ContainerLayout.jsx
+++ client/src/layouts/ContainerLayout.jsx
@@ -1,3 +1,3 @@
 export default function ContainerLayout({ children, className = "" }) {
-   return <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>{children}</div>;
+   return <div className={`max-w-6xl md:5x1 mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>{children}</div>;
 }
