189960384bb79b0f2d7b4d1a053227c384c3f695
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-17 10:51:32 +0200

    refactor articleCard to have individual styles and updating the styles

--- client/src/components/ui/ArticleCard.jsx
+++ client/src/components/ui/ArticleCard.jsx
@@ -1,16 +1,23 @@
-export default function ArticleCard({ article }) {
-   return (
-      <div className="bg-white shadow-md overflow-hidden flex flex-col transition hover:shadow-lg max-w-xs">
-         <img className="w-full h-64 object-cover" src={article.image} alt={article.title} />
-         <div className="p-3 flex flex-col flex-1">
-            <span className="text-xs text-[#212121] mb-2.5">Author: {article.email}</span>
-            <h2 className="text-2xl font-medium text-[#212121] line-clamp-2 leading-8 mb-1">{article.title}</h2>
-            <p className="min-h-15 text-sm text-[#212121] line-clamp-3 mb-2">{article.text}</p>
-            <span className="text-[#9A9A9A] text-xs text-right">· {article.date}</span>
-            <button className="w-36 text-black text-left leading-3.5 text-sm cursor-pointer bg-[#F4F4F4] py-3.5 px-3.5 font-medium uppercase hover:underline hover:bg-[#159e75] hover:text-white transition-colors duration-300 ease-in-out">
-               Read More <span className="ml-2">&rarr;</span>
-            </button>
-         </div>
-      </div>
-   );
+export default function ArticleCard({ img, author, title, date }) {
+  return (
+    <div className="shadow-md overflow-hidden flex flex-col transition hover:shadow-lg w-full mb-7">
+      <a href="">
+        <img
+          className="w-full h-64 object-cover hover:scale-105 transition-transform duration-300"
+          src={img}
+          alt="test-img"
+        />
+        <div className="m-3 flex flex-col flex-1">
+          <span className="text-xs text-[#212121] mb-2.5">
+            Author: {author}
+          </span>
+          <h2 className="text-lg font-medium text-[#212121] line-clamp-2 leading-8 mb-1">
+            {title}
+          </h2>
+
+          <span className="text-[#9A9A9A] text-xs text-right">{date}</span>
+        </div>
+      </a>
+    </div>
+  );
 }
