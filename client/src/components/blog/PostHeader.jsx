34c6e43620b0688fa9feea168972a8a598550e92
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-17 11:03:01 +0200

    rename PostHeading component

--- client/src/components/blog/PostHeader.jsx
+++ client/src/components/blog/PostHeader.jsx
@@ -1,10 +1,12 @@
-export default function PopularPosts({ heading, text }) {
-   return (
-      <>
-         <header className="mt-10 text-center px-4 pb-10">
-            <h1 className="text-3xl font-bold text-[#212121] leading-8 pb-2">{heading}</h1>
-            <p className="">{text}</p>
-         </header>
-      </>
-   );
+export default function PostHeader({ heading, text }) {
+  return (
+    <>
+      <header className="mt-15 text-center px-4 pb-10">
+        <h1 className="text-3xl font-bold text-[#212121] leading-8 pb-2">
+          {heading}
+        </h1>
+        <p className="">{text}</p>
+      </header>
+    </>
+  );
 }
