66e7d98308f038ea2e8be3253df4d98cd935574b
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-17 11:09:09 +0200

    adding authProvider for protected routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "@fontsource/inter";
 import App from "./App.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-   <StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-   </StrictMode>
+  //   <StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+  //   </StrictMode>
 );
