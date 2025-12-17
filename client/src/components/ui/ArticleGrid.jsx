511cb476ecb0ab7710369ed53248a9842ffd959c
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-17 10:52:39 +0200

    adding static ArticleCard image to the ArticleGrid parent

--- client/src/components/ui/ArticleGrid.jsx
+++ client/src/components/ui/ArticleGrid.jsx
@@ -1,13 +1,26 @@
 import ArticleCard from "./ArticleCard";
 
-export default function ArticleGrid({ articles }) {
-   return (
-      <div className="max-w-7xl mx-auto px-5 lg:px-24">
-         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-center">
-            {articles.map((article, idx) => (
-               <ArticleCard key={idx} article={article} />
-            ))}
-         </div>
-      </div>
-   );
+export default function ArticleGrid() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+      <ArticleCard
+        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        author="Test Person"
+        title="Ultimate Guide to Solo Travel"
+        date="Dec 15, 2025"
+      />
+      <ArticleCard
+        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        author="Test Person"
+        title="Ultimate Guide to Solo Travel"
+        date="Dec 15, 2025"
+      />
+      <ArticleCard
+        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        author="Test Person"
+        title="Ultimate Guide to Solo Travel"
+        date="Dec 15, 2025"
+      />
+    </div>
+  );
 }
