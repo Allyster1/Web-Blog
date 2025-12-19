8a898f77bd6719ddfd1d7cb1885511d415de15ff
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-20 01:05:57 +0200

    fixing footer path

--- client/src/layouts/BlogLayout.jsx
+++ client/src/layouts/BlogLayout.jsx
@@ -1,4 +1,4 @@
-import Footer from "../components/Footer";
+import Footer from "../components/footer/Footer";
 import Header from "../components/Header";
 import Pagination from "../components/Pagination";
 import LatestPosts from "../components/blog/LatestPosts";
