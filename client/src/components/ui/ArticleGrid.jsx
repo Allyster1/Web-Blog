c3a8ad1b0a01dcfd12e05b6e8ab9c03a0d3ef8d5
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-19 02:15:13 +0200

    adding more variety static images (temp)

--- client/src/components/ui/ArticleGrid.jsx
+++ client/src/components/ui/ArticleGrid.jsx
@@ -4,19 +4,19 @@ export default function ArticleGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
       <ArticleCard
-        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        img="https://images.unsplash.com/photo-1764778607908-3f115dc671ba?q=80&w=1123&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         author="Test Person"
         title="Ultimate Guide to Solo Travel"
         date="Dec 15, 2025"
       />
       <ArticleCard
-        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        img="https://images.unsplash.com/photo-1765834082429-93991ab1c38e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         author="Test Person"
         title="Ultimate Guide to Solo Travel"
         date="Dec 15, 2025"
       />
       <ArticleCard
-        img="https://images.unsplash.com/photo-1765120298918-e9932c6c0332?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        img="https://images.unsplash.com/photo-1765871320521-7eb7c98a1061?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         author="Test Person"
         title="Ultimate Guide to Solo Travel"
         date="Dec 15, 2025"
